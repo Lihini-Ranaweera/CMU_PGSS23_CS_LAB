608d9499f684f53ce58cba237a29744979673ce1
Lihini-Ranaweera <lranawee@andrew.cmu.edu>
2023-07-09 14:00:14 -0400

    Changes to the iris.R code

--- Week 1/iris.R
+++ Week 1/iris.R
@@ -1,5 +1,10 @@
+#install.packages("readr")
+#install.packages("ggplot2")
+#install.packages("cowplot")
+#install.packages("languageserver")
 library(readr)
-iris <- read_csv("/Users/pgmenon/Documents/CMU/PGSS2023/CMU_PGSS2023_CSLab/Week01/iris.csv", 
+
+iris <- read_csv("C:/Users/lihin/OneDrive/Documents/PGSS Computer Science Lab/CMU_PGSS23_CS_LAB/Week 1/iris.csv", 
                  col_names = FALSE, col_types = cols(X1 = col_double()))
 colnames(iris) <- c("Petal.Length", "Petal.Width", "Sepal.Length", "Sepal.Width", "Species")
 # View(iris)
@@ -18,4 +23,4 @@ print(table(iris$Species))
 library(ggplot2)
 library(cowplot)
 p1 <- ggplot(iris) + geom_boxplot(aes(x=Species, y=Sepal.Width, color=Species), outlier.alpha = 0.1) + theme_minimal()
-save_plot("/Users/pgmenon/Documents/CMU/PGSS2023/CMU_PGSS2023_CSLab/Week01/iris.png", p1, base_height=4, base_width=4, limitsize=FALSE)
+save_plot("C:/Users/lihin/OneDrive/Documents/PGSS Computer Science Lab/CMU_PGSS23_CS_LAB/Week 1/iris.png", p1, base_height=4, base_width=4, limitsize=FALSE)
